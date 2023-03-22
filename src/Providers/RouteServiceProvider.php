d7322a23b3c4d86ac22f7c9491d62d5cffc7489c
MD AL AMIN <mdalaminbey@gmail.com>
2023-03-22 22:32:08 +0600

    create ajax route functionality

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -3,9 +3,51 @@
 namespace WaxFramework\Providers;
 
 use WaxFramework\Contracts\Provider;
+use WaxFramework\App;
+use WaxFramework\Request\Route\DataBinder;
 
 class RouteServiceProvider extends Provider
 {
     public function boot() {
+        add_action( 'rest_api_init', [$this, 'action_rest_api_init'] );
+        add_action( 'init', [ $this, 'action_ajax_init' ] );
+    }
+
+    /**
+     * Fires after WordPress has finished loading but before any headers are sent.
+     */
+    public function action_ajax_init() : void {
+        $this->init_routes( 'ajax' );
+    }
+
+    /**
+     * Fires when preparing to serve a REST API request.
+     */
+    public function action_rest_api_init(): void {
+        $this->init_routes( 'rest' );
+    }
+
+    private function init_routes( string $type ) {
+        $data_binder = App::$container->get( DataBinder::class );
+
+        $namespace = App::$config->get( "app.{$type}_api.namespace" );
+
+        $data_binder->set_namespace( $namespace );
+
+        include App::get_dir( "routes/{$type}/api.php" );
+
+        $versions = App::$config->get( "app.{$type}_api.versions" );
+
+        if ( is_array( $versions ) ) {
+
+            foreach ( $versions as $version ) {
+                $version_file = App::get_dir( "routes/{$type}/{$version}.php" );
+
+                if ( is_file( $version_file ) ) {
+                    $data_binder->set_version( $version );
+                    include $version_file;
+                }
+            }
+        }
     }
 }
---
+++ src/Request/Route/Ajax.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace WaxFramework\Request\Route;
+
+use WaxFramework\App;
+use WaxFramework\Request\Response;
+use WP_REST_Request;
+
+class Ajax extends Route
+{
+    protected static $ajax_routes = [];
+
+    protected static function register_route( string $method, string $route, $callback, array $middleware = [] ) {
+        $action                    = static::get_final_route( $route );
+        $middleware                = array_merge( static::$group_middleware, $middleware );
+        static::$routes[$action][] = [
+            'method'     => $method,
+            'middleware' => $middleware,
+            'callback'   => $callback
+        ];
+        add_action( "wp_ajax_{$action}", [ self::class, 'action_wp_ajax' ] );
+        add_action( "wp_ajax_nopriv_{$action}", [ self::class, 'action_wp_ajax' ] );
+    }
+
+    /**
+     * Fires authenticated Ajax actions for logged-in users.
+     *
+     */
+    public static function action_wp_ajax() : void {
+        $action = $_REQUEST['action'];
+        $routes = static::$routes[$action];
+        $key    = array_search( $_SERVER['REQUEST_METHOD'], array_column( $routes, 'method' ) );
+        
+        if ( ! is_int( $key ) ) {
+            Response::set_headers( [], 404 );
+            echo wp_json_encode(
+                [
+                    'code'    => 'ajax_no_route', 
+                    'message' => 'No route was found matching the URL and request method.'
+                ] 
+            );
+            exit;
+        }
+
+        $route = $routes[$key];
+
+        $is_allowed = static::permission_callback( $route['middleware'] );
+
+        if ( ! $is_allowed ) {
+            Response::set_headers( [], 401 );
+            echo wp_json_encode(
+                [
+                    'code'    => 'ajax_forbidden', 
+                    'message' => 'Sorry, you are not allowed to do that.'
+                ] 
+            );
+            exit;
+        }
+
+        static::bind_wp_rest_request( $route['method'] );
+        $response = static::get_callback_response( $route['callback'] );
+
+        echo wp_json_encode( $response );
+        exit;
+    }
+
+    protected static function bind_wp_rest_request( string $method ) {
+
+        $wp_rest_request = new WP_REST_Request( $method, $_REQUEST['action'] );
+        $wp_rest_server  = new \WP_REST_Server;
+
+        $wp_rest_request->set_query_params( wp_unslash( $_GET ) );
+        $wp_rest_request->set_body_params( wp_unslash( $_POST ) );
+        $wp_rest_request->set_file_params( $_FILES );
+        $wp_rest_request->set_headers( $wp_rest_server->get_headers( wp_unslash( $_SERVER ) ) );
+        $wp_rest_request->set_body( $wp_rest_server->get_raw_data() );
+
+        App::$container->set( WP_REST_Request::class, $wp_rest_request );
+    }
+}
