2151f96731398dc87323b61d64356450c4acbe40
MD AL AMIN <mdalaminbey@gmail.com>
2023-03-27 17:02:25 +0600

    Update RouteServiceProvider.php

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -6,19 +6,19 @@ use WaxFramework\Contracts\Provider;
 use WaxFramework\App;
 use WaxFramework\Routing\Providers\RouteServiceProvider as WaxRouteServiceProvider;
 
-class RouteServiceProvider extends WaxRouteServiceProvider implements Provider
-{
-    protected static function init_routes( string $type ) {
+class RouteServiceProvider extends WaxRouteServiceProvider implements Provider {
+    public function boot() {
         parent::$container = App::$container;
 
-        $config = App::$config->get('app');
+        $config = App::$config->get( 'app' );
 
         parent::$properties = [
-            'rest' => $config['rest_api'],
-            'ajax' => $config['ajax_api'],
+            'rest'       => $config['rest_api'],
+            'ajax'       => $config['ajax_api'],
             'middleware' => $config['middleware'],
             'routes-dir' => App::get_dir( "routes" )
         ];
-        parent::init_routes($type);
+
+        parent::boot();
     }
 }
